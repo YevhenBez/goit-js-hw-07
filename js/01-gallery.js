03a7d27aac624cbd6bba8a25c3816af13523ba2d
YevhenBez <byxgalter@bigmir.net>
2023-02-18 16:38:11 +0200

    Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. (Завдання 1 Модуля 7)
    Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. (Завдання 1 Модуля 7)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+
+const imageMarkup = createImageCardMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createImageCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div>`
+    }).join('');
+}
